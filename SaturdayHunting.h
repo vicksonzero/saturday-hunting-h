b4dca9cf8555c31f84ae0bf92fa4d17d888174fd
Chui Hin Wah (Dickson) <dicksonZero@gmail.com>
2019-06-27 10:54:36 +0800

    Create SaturdayHunting.h

---
+++ SaturdayHunting.h
@@ -0,0 +1,17 @@
+"Saturday Hunting";
+
+
+"entity methods";
+"a=action";
+"b=entity object";
+"d=arguments";
+function f1: var_a, var_b, var_d, var_l {
+  if var_a == "create" {
+    var_i = var_d[0];
+    var_j = var_d[1];
+    "b=player object";
+    var_b = [[], [], [], 0];
+    "0=config"
+  
+  }
+}
